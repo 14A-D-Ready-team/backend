88bcf138a740f7a62ff7bd26574bcb8c4743e894
adamkekesi <kekesiadi>
2022-10-14 17:35:58 +0200

    a

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,8 @@ async function bootstrap() {
     .setVersion('1.0')
     .build();
 
+  config.asd();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
