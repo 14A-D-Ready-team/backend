caa5763e7ac8b5db40ac8f448b6c99e7e7a3d9d1
CircleCI Job <kekesi.adam@students.jedlik.eu>
2022-10-16 21:18:08 +0000

    Format code [skip ci]

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
-import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       cache: true,
       expandVariables: true,
-      envFilePath: ['.env', '.env.development'],
+      envFilePath: [".env", ".env.development"],
     }),
   ],
   controllers: [AppController],
