d0824edda16c8768c62a1c32cc48c523645abe28
adamkekesi <kekesiadi>
2022-10-22 21:11:39 +0200

    rebuildDbSchema

---
+++ src/shared/database/database.service.ts
@@ -0,0 +1,19 @@
+import { databaseConfig } from "./database.config";
+import { MikroORM } from "@mikro-orm/core";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export default class DatabaseService {
+  constructor(private orm: MikroORM) {}
+
+  public async rebuildDbSchema() {
+    const schemaGenerator = this.orm.getSchemaGenerator();
+    const dbName = this.orm.config.get("dbName");
+    if (!dbName) {
+      throw new Error("dbName is not defined in database config");
+    }
+    await schemaGenerator.dropDatabase(dbName);
+    await schemaGenerator.createDatabase(dbName);
+    await schemaGenerator.createSchema();
+  }
+}
