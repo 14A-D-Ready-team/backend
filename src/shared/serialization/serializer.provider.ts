690addbf8ef73a3fddd1c1ac47736e669cda3c99
Kékesi Ádám <kekesi.adam@students.jedlik.eu>
2022-11-03 14:15:17 +0100

    Upload

--- src/shared/serialization/serializer.provider.ts
+++ src/shared/serialization/serializer.provider.ts
@@ -1,8 +1,10 @@
 import { ClassSerializerInterceptor } from "@nestjs/common";
 import { APP_INTERCEPTOR } from "@nestjs/core";
+import { serializationConfig } from "./serialization.config";
 
 export const serializationInterceptorProvider = {
   provide: APP_INTERCEPTOR,
-  useFactory: (reflector: any) => new ClassSerializerInterceptor(reflector),
+  useFactory: (reflector: any) =>
+    new ClassSerializerInterceptor(reflector, serializationConfig),
   inject: ["Reflector"],
 };
