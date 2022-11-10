ab92be3408545e0b4cccfcc0b579cf0d71d43de7
Redstone12 <fekete.miklos@students.jedlik.eu>
2022-11-10 17:26:25 +0100

    user service test wip

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,32 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { UserService } from "./user.service";
+import { User } from "./entity";
+import { UserStatus } from "./enum";
+import { EmailDuplicateException } from "./duplicate-email.exeption";
+//import { TokenType } from "@/token";
+
+const mockUser = {
+  id: 1,
+  name: "John Doe",
+  email: "example@gmail.com",
+  password: "Supas3cr3t!",
+  type: 0,
+  status: UserStatus.Inactive,
+  //tokens: TokenType.Authentication,
+};
 
 describe("UserService", () => {
-  let service: UserService;
+  let service: any;
+  // let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        {
+          provide: UserService,
+          useValue: { create: jest.fn().mockResolvedValue("value") },
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
@@ -14,5 +34,21 @@ describe("UserService", () => {
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+    expect(service).toBeInstanceOf(UserService);
+  });
+
+  it("should create user", async () => {
+    service.create(mockUser).mockResolvedValue("value");
+    const result = await service.create(mockUser);
+
+    //service.create(mockUser);
+
+    expect(service.create(mockUser)).toBeCalledWith(mockUser);
+    expect(result).toEqual("value");
+  });
+
+  it("create hadles an error", () => {
+    service.create.mockResolvedValue(null);
+    expect(service.create()).rejects.toThrow(EmailDuplicateException);
   });
 });
