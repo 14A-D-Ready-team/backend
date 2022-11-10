bf5e84fdc64cf50e1b036dd72c76c7c0b69f86ed
Fekete Miklós <113507853+FeketeMiklos@users.noreply.github.com>
2022-11-10 09:55:44 +0100

    auth service, controller tests

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,18 +1,30 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { RegistrationDto } from "./dto";
 
 describe("AuthController", () => {
   let controller: AuthController;
+  let provider: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [{provide: AuthService, useValue: {signUp: jest.fn()}}],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
+    provider = module.get<AuthService>(AuthService);
   });
 
   it("should be defined", () => {
     expect(controller).toBeDefined();
+    expect(controller).toBeInstanceOf(AuthController);
+  });
+
+  it("should signup", () => {
+    const regDto = new RegistrationDto();
+    controller.signUp(regDto);
+    expect(provider.signUp).toBeCalledWith(regDto);
   });
 });
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,32 @@
+import { UserService } from "@/user";
 import { Test, TestingModule } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
+import { RegistrationDto } from "./dto";
 
 describe("AuthService", () => {
   let service: AuthService;
+  let provider: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        { provide: UserService, useValue: { create: jest.fn() } },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
+    provider = module.get<UserService>(UserService);
   });
 
   it("should be defined", () => {
     expect(service).toBeDefined();
+    expect(service).toBeInstanceOf(AuthService);
+  });
+
+  it("should signup", () => {
+    const regDto = new RegistrationDto();
+    service.signUp(regDto);
+    expect(provider.create).toBeCalledWith(regDto);
   });
 });
