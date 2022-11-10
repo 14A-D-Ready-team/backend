24a9c6adbb7d6c24f39970f30246f65149030a2c
CircleCI Job <ready-ci-bot@ready.com>
2022-11-10 10:34:46 +0000

    Format code [skip ci]

--- src/shared/session/session.middleware.spec.ts
+++ src/shared/session/session.middleware.spec.ts
@@ -1,7 +1,7 @@
-import { SessionMiddleware } from './session.middleware';
+import { SessionMiddleware } from "./session.middleware";
 
-describe('SessionMiddleware', () => {
-  it('should be defined', () => {
+describe("SessionMiddleware", () => {
+  it("should be defined", () => {
     expect(new SessionMiddleware()).toBeDefined();
   });
 });
