e921de82301b8ea5ab5e72b0b1b4315cdfc4f0cc
adamkekesi <kekesi.adam2003@gmail.com>
2022-11-13 17:03:30 +0100

    Setting session on google auth

--- src/auth/google-auth.controller.spec.ts
+++ src/auth/google-auth.controller.spec.ts
@@ -1,36 +1,44 @@
-import { TokenModule } from "./../token/token.module";
-import { MikroOrmModule } from "@mikro-orm/nestjs";
-import { DatabaseModule } from "./../shared/database/database.module";
-import { ConfigModule } from "@nestjs/config";
+import { User, UserType } from "@/user";
 import { Test, TestingModule } from "@nestjs/testing";
-import { authConfig } from "./auth.config";
+import { VerifyGoogleAuthDto } from "./dto";
 import { GoogleAuthController } from "./google-auth.controller";
 import { GoogleAuthService } from "./google-auth.service";
-import { User } from "@/user";
 
 describe("GoogleAuthController", () => {
   let controller: GoogleAuthController;
+  const userStub = new User();
 
   beforeEach(async () => {
-    /* const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        ConfigModule.forRoot({
-          cache: true,
-          envFilePath: [".env"],
-          load: [authConfig],
-        }),
-        DatabaseModule,
-        MikroOrmModule.forFeature([User]),
-        TokenModule
-      ],
+    userStub.id = 1;
+
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [],
       controllers: [GoogleAuthController],
-      providers: [GoogleAuthService],
+      providers: [
+        {
+          provide: GoogleAuthService,
+          useValue: {
+            verify: jest.fn().mockReturnValue(userStub),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<GoogleAuthController>(GoogleAuthController); */
+    controller = module.get<GoogleAuthController>(GoogleAuthController);
   });
 
   it("should be defined", () => {
-    expect(true).toBeDefined();
+    expect(controller).toBeDefined();
+  });
+
+  it("should return a user, and set session", async () => {
+    const payload = new VerifyGoogleAuthDto();
+    payload.token = "token";
+    payload.userType = UserType.Customer;
+    const session = {};
+
+    const result = await controller.verify(payload, session);
+    expect(result).toEqual(userStub);
+    expect(session).toEqual({ userId: userStub.id });
   });
 });
