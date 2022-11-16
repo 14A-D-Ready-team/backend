80d77ae3f82c92eb40adef97e3567d754a339f61
Kékesi Ádám <kekesi.adam@students.jedlik.eu>
2022-11-16 11:51:00 +0100

    upload

--- src/shared/policy/policies.guard.ts
+++ src/shared/policy/policies.guard.ts
@@ -1,11 +1,33 @@
 import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
 import { Observable } from "rxjs";
+import { policiesMetadataKey } from "./decorator";
+import { PolicyHandler } from "./policy.handler";
 
 @Injectable()
 export class PoliciesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
   public canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     return true;
+  }
+
+  private getPolicies(context: ExecutionContext): PolicyHandler[] {
+    const policiesController = this.reflector.get<PolicyHandler[]>(
+      policiesMetadataKey,
+      context.getClass(),
+    );
+    const policiesHandler = this.reflector.get<PolicyHandler[]>(
+      policiesMetadataKey,
+      context.getHandler(),
+    );
+
+    if (policiesHandler === undefined) {
+      return policiesController;
+    }
+
+    return [...policiesHandler, ...policiesController];
   }
 }
