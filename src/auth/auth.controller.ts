6a9d937f4e6edef69df7608b55e5597199f5d8df
Redstone12 <fekete.miklos@students.jedlik.eu>
2023-02-14 16:44:34 +0100

    add APIProperety and other stuff

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -15,11 +15,16 @@ import {
 } from "@/shared/swagger";
 import { InvalidDataException } from "@/shared/validation/exceptions";
 import {
+  EmailNotFoundException,
   InactiveUserException,
   InvalidLoginException,
+  InvalidTokenException,
   PasswordNotSetException,
 } from "./exceptions";
 import { EmailDto } from "./dto";
+import { InvalidPasswordException } from "./exceptions/invalid-password-exception";
+import { EmailDuplicateException } from "@/user/duplicate-email.exeption";
+import { InvalidIdException } from "@/shared/exceptions";
 
 @ApiTags("auth")
 @Controller("auth")
@@ -27,6 +32,16 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post("/signup")
+  @ApiResponse({ status: 200, type: User })
+  @BadRequestResponse(
+    InvalidDataException,
+    PasswordNotSetException,
+    EmailNotFoundException,
+    InvalidPasswordException,
+    EmailDuplicateException,
+  )
+  @ServiceUnavailableResponse()
+  @InternalServerErrorResponse()
   public async signUp(@Body() registrationDto: RegistrationDto) {
     return this.authService.signUp(registrationDto);
   }
@@ -51,33 +66,76 @@ export class AuthController {
   }
 
   @Post("/session-signin")
+  @ApiResponse({ status: 200, type: User })
+  @BadRequestResponse(
+    InvalidDataException,
+    InactiveUserException,
+    InvalidLoginException,
+  )
+  @ServiceUnavailableResponse()
+  @InternalServerErrorResponse()
   @Auth()
   public async sessionSignIn(@InjectAuthState() authState: AuthState) {
     return authState.user;
   }
 
   @Post("/logout")
+  @ApiResponse({ status: 200 })
+  @BadRequestResponse(
+    InvalidDataException,
+    InvalidLoginException,
+  )
+  @ServiceUnavailableResponse()
+  @InternalServerErrorResponse()
   @Auth()
   public async logout(@InjectAuthState() authState: AuthState) {
     await authState.logout();
   }
 
   @Post("/send-email-verification")
+  @ApiResponse({ status: 200 })
+  @BadRequestResponse(
+    EmailNotFoundException,
+  )
+  @ServiceUnavailableResponse()
+  @InternalServerErrorResponse()
   public async sendConfirmEmail(@Body() email: EmailDto) {
     return this.authService.sendConfirmEmail(email);
   }
 
   @Post("/send-password-reset")
+  @ApiResponse({ status: 200 })
+  @BadRequestResponse(
+    EmailNotFoundException,
+  )
+  @ServiceUnavailableResponse()
+  @InternalServerErrorResponse()
   public async sendPasswordResetEmail(@Body() email: EmailDto) {
     return this.authService.sendPasswordResetEmail(email);
   }
 
   @Post("/verify-user/:tokenId")
+  @ApiResponse({ status: 200, type: User })
+  @BadRequestResponse(
+    InvalidDataException,
+    InvalidTokenException,
+    InvalidIdException,
+  )
+  @ServiceUnavailableResponse()
+  @InternalServerErrorResponse()
   public async verifyUser(@Param("tokenId") tokenId: string) {
     return this.authService.verifyUser(tokenId);
   }
 
   @Post("/change-user-password/:tokenId")
+  @ApiResponse({ status: 200, type: User })
+  @BadRequestResponse(
+    InvalidDataException,
+    InvalidTokenException,
+    InvalidIdException,
+  )
+  @ServiceUnavailableResponse()
+  @InternalServerErrorResponse()
   public async changeUserPassword(
     @Param("tokenId") tokenId: string,
     @Body() newPasswordDto: NewPasswordDto,
