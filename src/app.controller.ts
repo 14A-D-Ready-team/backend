15990d2c61760b3abd632b013a089f36798ce896
Kékesi Ádám <kekesi.adam2003@gmail.com>
2023-03-09 08:57:28 +0100

    health check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get, Session } from "@nestjs/common";
+import { Controller, Get, HttpCode, Session } from "@nestjs/common";
 import { Expose, Transform } from "class-transformer";
 import { AppService } from "./app.service";
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @Get("/health")
+  @HttpCode(200)
+  public healthCheck() {}
 }
