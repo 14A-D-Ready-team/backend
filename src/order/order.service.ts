b16c7d3c2c97a71aa293a3103469f56468caf75d
Redstone12 <fekete.miklos@students.jedlik.eu>
2023-04-24 21:11:56 +0200

    getOrdersOfCustomer

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -14,9 +14,6 @@ export class OrderService {
   constructor(
     @InjectRepository(Order)
     private orderRepository: BaseRepository<Order>,
-
-    @InjectRepository(Buffet)
-    private buffetRepository: BaseRepository<Buffet>,
   ) {}
 
   public async place(payload: CreateOrderDto, user: User) {
@@ -24,12 +21,36 @@ export class OrderService {
       requestedPickupTime: payload.requestedPickup,
       customer: user.customer,
     });
-
+  
     order.statusHistory = new Collection(order, [
       new OrderStatus(OrderStatusEnum.Placed, new Date(), payload.message),
     ]);
-
+    
     await this.orderRepository.persistAndFlush(order);
     return order;
+  }
+
+  public findOne(id: number) {
+    return this.orderRepository.findOne(id);
+  }
+
+  public async getOrdersOfCustomer(customerId: number) {
+  //public async getOrdersOfCustomer(customerId: number, isInProgress: boolean) {
+    
+    // if (isInProgress === undefined) {
+    //   //
+    // }
+    // else if (isInProgress) {
+    //   //
+    // }
+    // else {
+    //   //
+    // }
+
+    //const orders = await this.orderRepository.find({ customer: { user: { id: customerId }}, statusHistory: { $contains: [ OrderStatusEnum.Done ] } });
+
+    const orders = await this.orderRepository.find({ customer: { user: { id: customerId }}});
+
+    return orders;
   }
 }
