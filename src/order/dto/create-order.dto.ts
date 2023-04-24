9d11c253b7cffd1d1961437f08940e2cccc929ee
CircleCI Job <ready-ci-bot@ready.com>
2023-04-24 07:39:58 +0000

    Format code [skip ci]

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,26 +1,31 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Expose } from "class-transformer";
-import { IsDate, IsNumber, IsString, MaxLength, MinLength } from "class-validator";
+import {
+  IsDate,
+  IsNumber,
+  IsString,
+  MaxLength,
+  MinLength,
+} from "class-validator";
 
 export class CreateOrderDto {
-    @Expose()
-    @ApiProperty()
-    @IsNumber()
-    public total: number;
-  
-    @Expose()
-    @ApiProperty()
-    @IsNumber()
-    public orderNumber: number;
-  
-    @Expose()
-    @ApiProperty()
-    @IsDate()
-    public date: Date;
+  @Expose()
+  @ApiProperty()
+  @IsNumber()
+  public total: number;
 
-    @Expose()
-    @ApiProperty()
-    @IsDate()
-    public pickupTime: Date;
+  @Expose()
+  @ApiProperty()
+  @IsNumber()
+  public orderNumber: number;
+
+  @Expose()
+  @ApiProperty()
+  @IsDate()
+  public date: Date;
+
+  @Expose()
+  @ApiProperty()
+  @IsDate()
+  public pickupTime: Date;
 }
-  
--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,6 +1,6 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Controller } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
 
 @ApiTags("order")
-@Controller('order')
+@Controller("order")
 export class OrderController {}
